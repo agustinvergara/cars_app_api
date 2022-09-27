4bd192601baf6849a010da5f63afd797331f453f
chuxi <agustin.vergara.ruiz@gmail.com>
2022-09-27 17:24:52 -0500

    NEW unit-model-serializer custom endpoint

--- cars_app_api/car_info_api/urls.py
+++ cars_app_api/car_info_api/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     #endpoint to send a json with all the brands
     path('brands/', views.cars_brand_serializer, name='all_brands_serializer'),
     #endpoint to send a json with all models on one brand
-    path('brand/<str:brand_id>/models/', views.cars_models_serializer, name='all_models_serializer'),
+    path('brand/<int:brand_id>/models/', views.cars_models_serializer, name='all_models_serializer'),
+    path('brand/model/<int:model_id>', views.car_model_unit_serializer, name='model_unit_serializer'),
 ]
--- cars_app_api/car_info_api/views.py
+++ cars_app_api/car_info_api/views.py
@@ -1,4 +1,5 @@
 import imp
+from symbol import return_stmt
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
@@ -29,3 +30,22 @@ def cars_models_serializer(request, brand_id):
     if request.method == 'GET' or 'POST':
         cars_models_serialized = CarsModelSerializer(brand_models, many=True)
         return Response(cars_models_serialized.data)
+ 
+@api_view(['GET','POST'])
+def car_model_unit_serializer(request, model_id):
+    car_model_queryset = Model.objects.select_related('brand').get(pk=model_id)
+    car_model_segment = Segment.objects.get(pk=car_model_queryset.segment)
+    model_unit_data = {
+        'model_id':car_model_queryset.model_id,
+        'brand_name':car_model_queryset.brand.brand_name,
+        'segment_name':car_model_segment.segment_name,
+        'model_name':car_model_queryset.model_name,
+        'model_image_1':car_model_queryset.model_image_1,
+        'model_image_2':car_model_queryset.model_image_2,
+        'model_image_3':car_model_queryset.model_image_3
+    }
+
+    if request.method == 'GET' or 'POST':
+        unit_model_serializer = UnitModelSerializer(data=model_unit_data)
+        unit_model_serializer.is_valid()
+        return Response(unit_model_serializer.data)
